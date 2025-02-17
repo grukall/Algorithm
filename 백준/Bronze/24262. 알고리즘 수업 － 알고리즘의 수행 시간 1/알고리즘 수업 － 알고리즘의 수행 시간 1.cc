41664595b9c85a125ee7dc78b89155c48ae76e81
이승언 <71816852+grukall@users.noreply.github.com>
2025-02-17 16:56:31 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.cc
@@ -0,0 +1,16 @@
+  #include <iostream>
+  #include <vector>
+  #include <algorithm>
+
+  using namespace std;
+
+int MenOfPassion(int *A, int n) {
+    int i = n / 2;
+    return A[i];
+}
+
+  int main()
+  {
+    cout << 1 << '\n';
+    cout << 0;
+  }
