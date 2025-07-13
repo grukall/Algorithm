4be00ca80a911a66c3797e9dbfb49cc9da1cbb72
이승언 <dobana12345@naver.com>
2025-07-13 10:49:06 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 248 ms, Memory: 13924 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void dfs(int n, vector<vector<int>> &graph, vector<int> &visited, int &count)
+{
+    visited[n] = count;
+    count += 1;
+
+    vector<int> &v = graph[n];
+    for (int next : v)
+    {
+        if (!visited[next])
+        {
+            dfs(next, graph, visited, count);
+        }
+    }
+}
+
+
+int main()
+{
+    int n, m, r; cin >> n >> m >> r;
+    vector<vector<int>> graph(n+1);
+    vector<int> visited(n+1, 0);
+
+    for (int i = 0; i < m; i++)
+    {
+        int u, v; cin >> u >> v;
+        graph[u].push_back(v);
+        graph[v].push_back(u);
+    }
+
+    for (vector<int> &v : graph)
+        sort(begin(v), end(v));
+
+    int count = 1;
+    dfs(r, graph, visited, count);
+
+    // 방문하지 않은 노드에 대해 0 출력
+    for (int i = 1; i <= n; ++i) {
+            cout << visited[i] << '\n';
+    }
+
+}
